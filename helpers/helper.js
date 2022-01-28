d024be091651c26bd0d0159f48fb9beefee993d6
Jose <jose_i11@hotmail.com>
2022-01-28 23:16:31 +0000

    add: removeNonAlphanumeric helper function

--- helpers/helper.js
+++ helpers/helper.js
@@ -2,4 +2,8 @@ function hasNonAlphanumeric(input) {
     return !/^[a-zA-Z0-9]*$/.test(input)
 }
 
-export { hasNonAlphanumeric }
+function removeNonAlphanumeric(input){
+    return input.replace(/[^a-zA-Z]/g, "");
+}
+
+export { hasNonAlphanumeric, removeNonAlphanumeric }

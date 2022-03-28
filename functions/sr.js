e478e3b8d706991b340e67c8f236fbf5554c3e68
Jose <jose_i11@hotmail.com>
2022-03-28 21:49:49 +0100

    fix: default emoteset

--- functions/sr.js
+++ functions/sr.js
@@ -1,30 +1,56 @@
+const emotes = {
+    default: {
+        slot1: "OSFrog",
+        slot2: "OhMyDog ",
+        slot3: "CoolCat",
+        slotsWin: "bttvNice", 
+        slotsLoss: "Jebasted",
+        dice1: "PoroSad",
+        dice2: "DansGame",
+        dice3: "NinjaGrumpy",
+        dice4: "CaitlynS",
+        dice5: "GingerPower",
+        dice6: "MVGame",
+        dice7: "PotFriend",
+        dice8: "Poooound",
+        dice9: "PogChamp",
+        dice10: "PogBones",
+    },
+}
+
 function getSR(channel) {
     const channelName = channel.substring(1)
     const sr = Math.floor(Math.random() * 4850) + 1
     let emote
+    let emoteset
 
-    if (!emotes[channelName]) return
+    if (!emotes[channelName]) {
+        emoteset = emotes['default']
+    } else {
+        emoteset = emotes[channelName]
+    }
+    
 
     if (sr <= 500) {
-        emote = emotes[channelName].dice1
+        emote = emoteset.dice1
     } else if (sr > 500 && sr <= 1000) {
-        emote = emotes[channelName].dice2
+        emote = emoteset.dice2
     } else if (sr > 1000 && sr <= 1500) {
-        emote = emotes[channelName].dice3
+        emote = emoteset.dice3
     } else if (sr > 1500 && sr <= 2000) {
-        emote = emotes[channelName].dice4
+        emote = emoteset.dice4
     } else if (sr > 2000 && sr <= 2500) {
-        emote = emotes[channelName].dice5
+        emote = emoteset.dice5
     } else if (sr > 2500 && sr <= 3000) {
-        emote = emotes[channelName].dice6
+        emote = emoteset.dice6
     } else if (sr > 3000 && sr <= 3500) {
-        emote = emotes[channelName].dice7
+        emote = emoteset.dice7
     } else if (sr > 3500 && sr <= 4000) {
-        emote = emotes[channelName].dice8
+        emote = emoteset.dice8
     } else if (sr > 4000 && sr <= 4500) {
-        emote = emotes[channelName].dice9
+        emote = emoteset.dice9
     } else if (sr > 4500) {
-        emote = emotes[channelName].dice10
+        emote = emoteset.dice10
     }
     return {
         sr,

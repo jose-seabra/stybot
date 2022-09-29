705a9d1fe752b78eede9a951db488e50ee4e9741
jose-seabra <jose_i11@hotmail.com>
2022-09-29 12:53:44 +0100

    cleanup: cleaner exports

--- helpers/helper.js
+++ helpers/helper.js
@@ -1,21 +1,19 @@
-function hasNonAlphanumeric(input) {
+export function hasNonAlphanumeric(input) {
     return !/^[a-zA-Z0-9]*$/.test(input)
 }
 
-function removeNonAlphanumeric(input) {
+export function removeNonAlphanumeric(input) {
     return input.replace(/[^a-zA-Z]/g, "")
 }
 
-function sleep(millis) {
+export function sleep(millis) {
     return new Promise((resolve) => setTimeout(resolve, millis))
 }
 
-function shuffleArray(array) {
+export function shuffleArray(array) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1))
         ;[array[i], array[j]] = [array[j], array[i]]
     }
     return array
 }
-
-export { hasNonAlphanumeric, removeNonAlphanumeric, sleep, shuffleArray }

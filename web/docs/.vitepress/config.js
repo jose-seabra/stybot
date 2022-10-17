a776d4a883e1cb8d99c68f9a82091bc47e5cbc95
Jose Mario <jose_i11@hotmail.com>
2022-10-17 12:06:57 +0100

    docs: vitepress

---
+++ web/docs/.vitepress/config.js
@@ -0,0 +1,16 @@
+export default {
+    title: 'Home',
+    titleTemplate: 'Stybot',
+    base: '/stybot/',
+    themeConfig: {
+        sidebar: [
+            {
+              text: 'Commands',
+              items: [
+                { text: 'Page 1', link: '/1' },
+                { text: 'Page 2', link: '/2' },
+              ]
+            }
+          ]
+      }
+}

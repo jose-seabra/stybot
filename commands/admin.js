92994645c2ec6e8c7eb32a77232abebd160f67d6
jose-seabra <jose_i11@hotmail.com>
2023-07-23 00:35:32 +0100

    fix: now for sure?

--- commands/admin.js
+++ commands/admin.js
@@ -43,7 +43,7 @@ function update(chatClient, channel, user, msg, args) {
     const [command, option] = args
 
     exec("git pull", (error, stdout, stderr) => {
-        if (error) {
+        if (error && error.message) {
             let errorMessage
             switch (option) {
                 case "-v":

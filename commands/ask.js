c2dfb3dc737537792df1121694fbb0f89c09761d
jose-seabra <jose_i11@hotmail.com>
2023-09-13 21:06:07 +0100

    fix: openai v4 breaking changes

--- commands/ask.js
+++ commands/ask.js
@@ -3,29 +3,28 @@ import { settings } from "../settings/ask.js"
 let status = {}
 
 import { readyToRun } from "../helpers/commandHandler.js"
-import { Configuration, OpenAIApi } from "openai"
+import OpenAI from "openai"
 
-const configuration = new Configuration({
+const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 })
-const openai = new OpenAIApi(configuration)
 
 export async function ask(chatClient, channel, user, msg, args) {
     readyToRun(settings, status, channel, user, msg)
         .then(async () => {
             const q = args.join(" ")
 
-            const safe = await openai.createModeration({
+            const safe = await openai.moderations.create({
                 mode: "text-moderation-latest",
                 input: q,
             })
 
-            if (safe.data.results[0].flagged) {
+            if (safe.results[0].flagged) {
                 chatClient.say(channel, "Don't be weird Dudge")
                 return
             }
 
-            const response = await openai.createCompletion({
+            const response = await openai.completions.create({
                 model: "text-davinci-002",
                 prompt: `Stybot is a chatbot that answers questions with funny and controversial responses:\n\nYou: ${q}\nStybot:`,
                 temperature: 1,
@@ -34,7 +33,7 @@ export async function ask(chatClient, channel, user, msg, args) {
                 frequency_penalty: 0.5,
                 presence_penalty: 0,
             })
-            chatClient.say(channel, response.data.choices[0].text)
+            chatClient.say(channel, response.choices[0].text)
         })
         .catch((error) => {})
 }
